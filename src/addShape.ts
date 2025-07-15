8eaaaabcac2d9d085c00c0aa5444d1e4fd09d1ae
jobo322 <jose.bolanos@correounivalle.edu.co>
2025-07-15 14:53:19 -0500

    feat: addShape function

--- src/addShape.ts
+++ src/addShape.ts
@@ -1,20 +1,37 @@
-import { getShape1D, Shape1D } from 'ml-peak-shape-generator';
+import { Shape1D } from 'ml-peak-shape-generator';
 import { Signal, SignalWithShape } from './types';
 
-export function addShape(
-  signalInput: Signal<Shape1D>,
-  shape: Shape1D = { kind: 'gaussian' },
-): SignalWithShape<Shape1D> {
-  const signal = structuredClone(signalInput);
-  if (!signal.shape) {
-    signal.shape = shape;
-  } else {
-    signal.shape = { ...shape, ...signal.shape };
-  }
+export function addShape<T extends Shape1D = Shape1D>(
+  shape: T,
+  signalInput: Signal<T>,
+): SignalWithShape<T>;
+export function addShape<T extends Shape1D = Shape1D>(
+  shape: T,
+  signalInput?: undefined,
+): (signal: Signal<T>) => SignalWithShape<T>;
+export function addShape<T extends Shape1D = Shape1D>(
+  shape: T,
+  signalInput?: Signal<T>,
+): SignalWithShape<T> | ((signal: Signal<T>) => SignalWithShape<T>) {
+  const fct: (signal: Signal<T>) => SignalWithShape<T> = (
+    signalInput: Signal<T>,
+  ) => {
+    const signal = structuredClone(signalInput);
+    if (!signal.shape) {
+      signal.shape = shape;
+    } else {
+      signal.shape = { ...shape, ...signal.shape };
+    }
 
-  return {
-    ...signal,
-    shape: { ...shape, ...signal.shape },
-    constants: [],
+    return {
+      ...signal,
+      shape: { ...shape, ...signal.shape },
+      constants: [],
+    };
   };
+  if (signalInput) {
+    return fct(signalInput);
+  } else {
+    return fct;
+  }
 }
--- src/types.ts
+++ src/types.ts
@@ -12,6 +12,7 @@ interface SignalPattern {
 }
 
 interface ParametersLimits {
+  init: number;
   min: number;
   max: number;
   gradientDifference: number;
@@ -22,27 +23,36 @@ type ConstantsObject<T extends { kind: string }> =
       [K in T['kind']]: keyof Omit<Extract<T, { kind: K }>, 'kind'>;
     };
 
-export type ParametersObject<T extends { kind: string }> = {
-  [K in T['kind']]: Record<
-    keyof Omit<Extract<T, { kind: K }>, 'kind'>,
-    ParametersLimits
-  >;
-} & Record<'x' | 'y', ParametersLimits>;
+export type Parameters<T extends { kind: string }> =
+  | 'x'
+  | 'y'
+  | {
+      [K in T['kind']]: keyof Omit<Extract<T, { kind: K }>, 'kind'>;
+    }[T['kind']];
 
-type Parameters<T extends { kind: string }> = keyof ParametersObject<T>;
+export type ParametersObject<T extends { kind: string }> = Record<
+  Parameters<T>,
+  ParametersLimits
+>;
+
 type SignalParameters<T extends { kind: string }> = Partial<
-  Record<Parameters<T>, ParametersLimits>
+  Record<Parameters<T>, Partial<ParametersLimits>>
 >;
 
-export type ParameterNames<T extends { kind: string }> = Array<
-  ConstantsObject<T>[T['kind']] | 'x' | 'y'
->;
+export type ParameterName<T extends { kind: string } = Shape1D> =
+  | ConstantsObject<T>[T['kind']]
+  | 'x'
+  | 'y';
 
-export type Signal<T extends Shape1D> = {
+export type ParameterNames<T extends { kind: string } = Shape1D> =
+  ParameterName<T>[];
+
+export type Signal<T extends Shape1D = Shape1D> = {
   x: number;
   y: number;
   shape?: T;
   pattern?: SignalPattern[];
+  parameters?: SignalParameters<T>;
   constants?: Array<ConstantsObject<Shape1D>[T['kind']] | 'x' | 'y'>;
 };
 
@@ -51,6 +61,7 @@ export type SignalWithShape<T extends Shape1D> = {
   y: number;
   shape: T;
   pattern?: SignalPattern[];
+  parameters?: SignalParameters<T>;
   constants: Array<ConstantsObject<Shape1D>[T['kind']] | 'x' | 'y'>;
 };
 
@@ -66,6 +77,6 @@ export type InternalSignal<T extends Shape1D> = {
   propertiesValues: SignalParameters<T>;
 };
 
-export type GetInternalSignalsOptions<T extends Shape1D> = {
+export type GetInternalSignalsOptions<T extends Shape1D = Shape1D> = {
   parameters?: SignalParameters<T>;
 };
